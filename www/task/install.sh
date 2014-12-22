4adb5e40d858e4ef8d86f1e45f34a152e32cf8c5
xiaowu <admin@xuexb.com>
2014-12-23 00:07:11 +0800

    添加404，修复不能发文章，添加内容管理按钮，添加管理入口

---
+++ www/task/install.sh
@@ -0,0 +1,5 @@
+﻿echo "==================================";
+echo  "开始安装...";
+echo  "(安装过程可能会有报错，无视即可）";
+echo "==================================";
+cd ../ && npm install && cp -f ./task/grunt-css-sprite/sprite.js ./node_modules/grunt-css-sprite/tasks/sprite.js && echo "安装完成"
