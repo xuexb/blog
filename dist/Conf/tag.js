8fd9e24b82fe66298a784b5d27099d887c3642ca
xuexb <admin@xuexb.com>
2015-05-31 22:29:06 +0800

    修复bug

--- dist/Conf/tag.js
+++ dist/Conf/tag.js
@@ -1,2 +1,2 @@
-/*xuexb_blog - v0.0.1 - 2015-05-31  21:05:23*/
+/*xuexb_blog - v0.0.1 - 2015-05-31  22:05:52*/
 "use strict";module.exports={app_begin:[function(a){"Home"===a.group&&a.getHeader("user-agent").toLowerCase().match(/applewebkit.*mobile.*/)&&(a.group="Touch")}]};
