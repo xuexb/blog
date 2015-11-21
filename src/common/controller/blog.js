0b5c48195c4ccd87653a0bf5d020c2317f84c7c5
xuexb <fe.xiaowu@gmail.com>
2015-11-21 11:11:21 +0800

    添加ls配置 [blog compile]

--- src/common/controller/blog.js
+++ src/common/controller/blog.js
@@ -5,6 +5,9 @@
 
 'use strict';
 
+import fs from 'fs';
+import path from 'path';
+
 export default class extends think.controller.base {
 
     /**
@@ -89,23 +92,106 @@ export default class extends think.controller.base {
      * 初始化ls
      */
     async init_ls() {
+        /**
+         * ls的缓存对象
+         *
+         * @example
+         *     {
+         *         js: {
+         *             key: {
+         *                 uri: '',
+         *                 md5: '',
+         *             }
+         *         }
+         *     }
+         * @type {Object}
+         */
         let ls_data = await think.cache('ls_data');
 
+        // 如果没有缓存
         if (think.isEmpty(ls_data)) {
-            ls_data = {css: {}, js: {}};
-            
+            ls_data = {
+                js: await this.init_ls_data('js'),
+                css: await this.init_ls_data('css')
+            };
+
+            await think.cache('ls_data', ls_data, {
+                timeout: 60 * 60 * 24 * 30
+            });
         }
 
-        // console.log(ls_data);
+        this.config('ls__cache', ls_data)
+    }
+
+    /**
+     * 初始化ls文件
+     *
+     * @param  {string} type 类型， css||js
+     *
+     * @return {Object}      配置对象
+     */
+    async init_ls_data(type = 'js') {
+        let result = {};
+        let config_ls = this.config('blog.ls');
+        let data = config_ls[type];
+
+        if(think.isEmpty(data)){
+            return result;
+        }
+
+        Object.keys(data).forEach(key => {
+            // 文件路径
+            let filepath = path.resolve(think.ROOT_PATH, data[key]);
+
+            // 如果该文件不存在
+            if (!fs.existsSync(filepath)) {
+                return;
+            }
+
+            // 获取文件内容
+            let filedata = fs.readFileSync(filepath).toString();
+
+            result[key] = {
+                uri: filepath,
+                md5: think.md5(filedata).substr(0, 6)
+            } 
+        });
+
+        return result;
+    }
+
+    ls(id, type = 'js') {
+        let ls_data = this.config('ls__cache')[type];
+        if (!id || !ls_data[id]) {
+            return '';
+        }
+
+        ls_data = ls_data[id];
+        let cookie_data = this.cookie(type + '_'+ id);
+        let data = fs.readFileSync(ls_data.uri).toString();
+        let html;
+
+        if (!cookie_data || cookie_data !== ls_data.md5) {
+            if (type === 'js') {
+                html = `<script id="${type}_${id}">${data}</script>`;
+            } else {
+                html = `<style id="${type}_${id}">${data}</style>`;
+            }
+            html += `<script>LS.set('${type}_${id}', '${ls_data.md5}')</script>`;
+        } else {
+            html = `<script>LS.get('${type}_${id}', '${type}')</script>`;
+        }
+
+        return html;
     }
 
     /**
      * 前置
      */
-    __before() {
-        this.init_compress();
+    async __before() {
+        await this.init_compress();
 
-        // this.init_ls();
+        await this.init_ls();
     }
 
     /**
---
+++ www/static/src/ls.js
@@ -0,0 +1,113 @@
+/**
+ * @file 好搜首页缓存模拟
+ * @description 只是玩玩，代码没有考虑过多的场景
+ * @author xiaowu
+ * @email fe.xiaowu@baidu.com
+ */
+
+(function (window, document) {
+    'use strict';
+
+    /**
+     * 暴露空间
+     * @type {Object}
+     */
+    var LS = window.LS = {
+        set: function () {},
+        get: function () {}
+
+    };
+
+    /**
+     * 别名
+     * @type {Object}
+     */
+    var cname = {
+        js: 'script',
+        css: 'style'
+
+    };
+
+    /**
+     * 写入cookie
+     *
+     * @param  {string} key   名
+     * @param  {string} value 值,删除则是null
+     */
+    var setcookie = function (key, value) {
+        var time = value === null ? -1 : 999;
+        var expires = '; expires=' + new Date(+new Date + 864e5 * time).toGMTString();
+        document.cookie = key + '=' + value + '; path=/;'+ expires;
+    };
+
+    /**
+     * 使用数据插入到页面head中
+     *
+     * @param  {string} tagName 标签名
+     * @param  {string} data    数据
+     */
+    var append = function (tagName, data) {
+        var div = document.createElement(tagName);
+        div.innerHTML = data;
+        document.head.appendChild(div);
+        div = null;
+    };
+
+    /**
+     * 读取缓存并写入到页面中
+     *
+     * @param  {string} id   缓存id
+     * @param  {string} type 缓存类型，有js,css
+     */
+    var cache2html = function (id, type) {
+        var data = localStorage[id];
+
+        // 如果缓存里没有数据
+        if (!data) {
+            setcookie(id, null);
+            return location.reload(true);
+        }
+
+        // 把数据写入到页面中
+        append(cname[type], data);
+    };
+
+    /**
+     * 写入缓存
+     *
+     * @param  {string} id 元素id
+     * @param {string} md5 md5值
+     */
+    var html2cache = function (id, md5) {
+        var elem = document.getElementById(id);
+        var innerHTML;
+
+        // 如果没有元素
+        if (!elem) {
+            return;
+        }
+
+        innerHTML = elem.innerHTML.trim();
+
+        // 写入缓存
+        localStorage[id] = innerHTML;
+
+        // 写入后再读取，验证是否写入成功
+        if (localStorage[id] === innerHTML) {
+            setcookie(id, md5);
+        } else {
+            setcookie(id, null);
+            delete localStorage[id];
+        }
+    };
+
+    // 如果支持缓存
+    // 为了防止禁用cookie时的报错
+    try {
+        if (window.localStorage && navigator.cookieEnabled) {
+            LS.get = cache2html;
+            LS.set = html2cache;
+        }
+    }
+    catch (e) {}
+})(window, document);
