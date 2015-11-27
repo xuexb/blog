66be8749bc152deb367f4cbdf48ce59f071ac36e
xuexb <fe.xiaowu@gmail.com>
2015-11-27 10:48:01 +0800

    优化code [blog compile]

--- src/common/config/env/development.js
+++ src/common/config/env/development.js
@@ -2,14 +2,11 @@
 
 export default {
     db: {
-        cache: {
-            on: false
-        },
-        host: '192.168.3.102',
+        host: 'www.xuexb.com',
         port: '3306',
         name: 'xuexb',
         user: 'root',
-        pwd: 'test',
+        pwd: 'iWENLING918LIANG',
         prefix: ''
     },
 
