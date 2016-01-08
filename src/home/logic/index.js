b9382d3743a5cefd99c36a72b16f5b948e201181
xuexb <fe.xiaowu@gmail.com>
2016-01-08 23:49:49 +0800

    添加主页浏览更多博文 [blog compile]

--- src/home/logic/index.js
+++ src/home/logic/index.js
@@ -5,11 +5,13 @@
  * @return {}     []
  */
 export default class extends think.logic.base {
-  /**
-   * index action logic
-   * @return {} []
-   */
-  indexAction(){
-   
-  }
+    /**
+     * index action logic
+     * @return {} []
+     */
+    indexAction() {
+        this.rules = {
+            page: 'int|min:1|default:1'
+        }
+    }
 }
