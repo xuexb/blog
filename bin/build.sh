17e39fe38dc1e890d8f8ef8c2f364d28e8aaba9f
xuexb <fe.xiaowu@gmail.com>
2017-04-15 02:00:27 +0800

    修改编译

--- bin/build.sh
+++ bin/build.sh
@@ -1,45 +1,32 @@
 #!/bin/sh
 
-rm -rf firekylin;
 rm -rf output;
-rm -rf output.theme;
-
 mkdir output;
+
+cp .thinkjsrc output/
+cp .installed output/
+
+echo 'copy www/* start ...';
+mkdir output/www;
+cp -r www/* output/www;
+rm -rf output/www/static;
+echo 'copy www/* end';
 
 echo 'webpack start ...';
 npm run webpack.build.production;
 echo 'webpack end';
 
+echo 'stc start ...';
 node stc.config.js;
-
-mkdir -p www/theme/firekylin.build/html;
-cp -r www/theme/firekylin/*.html www/theme/firekylin.build/html/
-cp -r www/theme/firekylin/inc www/theme/firekylin.build/html/
-cp -r www/theme/firekylin/template www/theme/firekylin.build/html/
-cp -r www/theme/firekylin/error www/theme/firekylin.build/html/
-cp -r www/theme/firekylin/package.json www/theme/firekylin.build/html/
-
-node stc.view.config.js;
-
-cp -r output.theme/www/theme/firekylin.build/html/* output.theme/www/theme/firekylin;
-rm -rf output.theme/www/theme/firekylin.build;
-cp -r output.theme/www/ output/www/
-rm -rf output.theme;
-rm -rf www/theme/firekylin.build/;
-
+echo 'stc end';
 
 npm run compile;
 npm run copy-package;
 
+# 复制app
 cp -r app output;
+
+# 删除没用文件
 rm -rf output/app/common/runtime;
-
-cp -r www/*.js output/www;
-
-
-rm -r output/app/common/config/db.js;
-rm -rf output/www/static/js/*.map;
-
-mv output dist;
-cp .thinkjsrc dist/
-cp .installed dist/
+rm -rf output/www/static/admin/js/*.map;
+rm -rf output/app/*.map;
--- stc.config.js
+++ stc.config.js
@@ -1,26 +1,41 @@
 var stc = require('stc');
-var htmlCompress = require('stc-html-compress');
-var uglify = require('stc-uglify');
 var cssCompress = require('stc-css-compress');
 var resourceVersion = require('stc-resource-version');
+var localstorage = require('stc-localstorage');
+var localstorageAdapter = require('stc-localstorage-nunjucks');
+var cssCombine = require('stc-css-combine');
+var htmlCompress = require('stc-html-compress');
+var uglify = require('stc-uglify');
+
 
 
 stc.config({
-  product: 'firekylin',
+  product: 'xuexb.com',
   include: ['view/', 'www/static/'],
   exclude: [/static\/admin\/src/, /static\/upload/],
+  outputPath: 'output',
   tpl: {
     engine: 'nunjucks',
     ld: ['{%', '{{', '{#'],
-    rd: ['%}', '}}', '#}']
+    rd: ['%}', '}}', '#}'],
   }
 });
 
 stc.workflow({
   uglify: {plugin: uglify, exclude: [/static\/admin\/src/]},
+  cssCombine: {plugin: cssCombine, include: /\.css$/},
   cssCompress: {plugin: cssCompress},
-  resourceVersion: {plugin: resourceVersion},
-  htmlCompress: {plugin: htmlCompress}
+  localstorage: {
+    include: {type: 'tpl'},
+    plugin: localstorage,
+    options: {
+      adapter: localstorageAdapter,
+      minLength : 200,
+      appId : '3e988cdb'
+    }
+  },
+  htmlCompress: {plugin: htmlCompress},
+  resourceVersion: {plugin: resourceVersion}
 });
 
 stc.start();
