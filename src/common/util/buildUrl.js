938d78bb47df56efd93c4a5ea5ef2711b4f5799d
xuexb <fe.xiaowu@gmail.com>
2017-04-21 02:14:05 +0800

    fix bug

--- src/common/util/buildUrl.js
+++ src/common/util/buildUrl.js
@@ -1,6 +1,5 @@
 export default {
   amp(content, prefix = '') {
-    console.log(prefix)
     return content.replace(/href="(?:https?:\/\/(www\.)?xuexb\.com\/|\/)(|[^\/].*?)"/gi, ($0, $1, $2) => {
       return `href="${prefix}/${$2}"`;
     });
