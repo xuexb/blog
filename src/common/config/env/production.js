0cb39e5699998878d96ad4419f578f3721447d5d
xuexb <fe.xiaowu@gmail.com>
2017-04-21 02:11:29 +0800

    修改配置

--- src/common/config/env/production.js
+++ src/common/config/env/production.js
@@ -3,5 +3,6 @@
 export default {
   cluster_on: 1,
   mip_site_url: 'https://mip.xuexb.com',
-  amp_site_url: 'https://amp-xuexb-com.cdn.ampproject.org/c/s/amp.xuexb.com'
+  // amp_site_url: 'https://amp-xuexb-com.cdn.ampproject.org/c/s/amp.xuexb.com'
+  amp_site_url: 'https://amp.xuexb.com'
 };
