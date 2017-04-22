7841adc30d5eb712ca4f5b1f4a28464f54fd22fe
xuexb <fe.xiaowu@gmail.com>
2017-04-23 03:38:34 +0800

    修复链接

--- src/common/util/buildUrl.js
+++ src/common/util/buildUrl.js
@@ -1,6 +1,6 @@
 export default {
   amp(content, prefix = '') {
-    return content.replace(/href="(?:https?:\/\/(www\.)?xuexb\.com\/|\/)(|[^\/].*?)"/gi, ($0, $1, $2) => {
+    return content.replace(/href="(?:https?:\/\/(www\.)?xuexb\.com\/?|\/)(|[^\/].*?)"/gi, ($0, $1, $2) => {
       return `href="${prefix}/${$2}"`;
     });
   }
