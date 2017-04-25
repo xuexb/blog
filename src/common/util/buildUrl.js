6510cf2f4cd62e7a791a7dafbda9fcaab1427e94
xuexb <fe.xiaowu@gmail.com>
2017-04-25 23:40:59 +0800

    添加mip链接适配

--- src/common/util/buildUrl.js
+++ src/common/util/buildUrl.js
@@ -1,7 +1,17 @@
+const mipReg = /<a\s+(.*?)href="(?:https?:\/\/(www\.)?xuexb\.com\/?|\/)(|[^\/].*?)"\s*(.*?)>([\s\S]+?)<\/a>/gi;
+const ampReg = /href="(?:https?:\/\/(www\.)?xuexb\.com\/?|\/)(|[^\/].*?)"/gi;
+
 export default {
-  amp(content, prefix = '') {
-    return content.replace(/href="(?:https?:\/\/(www\.)?xuexb\.com\/?|\/)(|[^\/].*?)"/gi, ($0, $1, $2) => {
-      return `href="${prefix}/${$2}"`;
-    });
-  }
+    amp(content, prefix = '') {
+        return content.replace(ampReg, ($0, $1, pathname) => {
+            return `href="${prefix}/${pathname}"`;
+        });
+    },
+
+    mip(content, prefix = '') {
+        return content.replace(mipReg, ($0, $1, url, pathname, placeholder, text) => {
+            text = String(text || '').replace(/[\n\r]\s*/g, '').trim();
+            return `<mip-link href="${prefix}/${pathname}" title="${text}">${text}</mip-link>`;
+        });
+    }
 };
