08d487b54c825025c907fecdaeb41ceb30e85534
前端小武 <fe.xiaowu@gmail.com>
2017-05-12 00:00:23 +0800

    关闭webp图片 (#66)
    * 优化占位图片
    
    * 使用七牛云图片在线压缩
    
    * 修复位置判断
    
    * 添加图片加载动画效果
    
    * 添加注释, 关闭webp图片

--- src/common/util/buildImg.js
+++ src/common/util/buildImg.js
@@ -1,29 +1,70 @@
+/**
+ * 图片占位符
+ *
+ * @type {tring}
+ */
 const placeholder = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
 
 export default {
-  mip(content) {
-    return content.replace(/<img\s+(.+?)\/?>/g, '<mip-img layout="responsive" $1></mip-img>');
-  },
+    /**
+     * 输出mip规范的img标签
+     *
+     * @param  {string} content 内容
+     *
+     * @return {string}
+     */
+    mip(content) {
+        return content.replace(/<img\s+(.+?)\/?>/g, '<mip-img layout="responsive" $1></mip-img>');
+    },
 
-  amp(content) {
-    return content.replace(/<img\s+(.+?)\/?>/g, '<amp-img layout="responsive" $1></amp-img>');
-  },
+    /**
+     * 输出amp规范的img标签
+     *
+     * @param  {string} content 内容
+     *
+     * @return {string}
+     */
+    amp(content) {
+        return content.replace(/<img\s+(.+?)\/?>/g, '<amp-img layout="responsive" $1></amp-img>');
+    },
+    /**
+     * 输出主站带有占位+延迟加载的img标签
+     *
+     * @param  {string} content 内容
+     *
+     * @return {string}
+     */
+    lazy(content) {
+        return content.replace(/<img\s+src="([^"]+)"\s*(.*?)width="(\d+)"\s+height="(\d+)"\/?>/g, (all, src, alt, width, height) => {
+            return `<div class="img" style="width: ${width}px;"><i style="padding-bottom: ${height / width * 100}%"></i><img src="${placeholder}" data-src="${src}" class="lazy-load"></div>`;
+        });
+    },
 
-  lazy(content) {
-    return content.replace(/<img\s+src="([^"]+)"\s*(.*?)width="(\d+)"\s+height="(\d+)"\/?>/g, (all, src, alt, width, height) => {
-      return `<div class="img" style="width: ${width}px;"><i style="padding-bottom: ${height / width * 100}%"></i><img src="${placeholder}" data-src="${src}" class="lazy-load"></div>`;
-    });
-  },
+    /**
+     * 内容里图片链接转webp或者七牛图片压缩
+     *
+     * @param  {string}  content 内容
+     * @param  {boolean} isWebp  是否支持webp
+     *
+     * @return {string}
+     */
+    toWebp(content, isWebp) {
+        const imgstr = isWebp ? 'imageMogr2/format/webp' : 'imageslim';
+        return content.replace(/<img\s+(.*?)src="([^"]+)"/g, (all, $0, src) => {
+            return `<img src="${src}?${imgstr}" ${$0}`;
+        });
+    },
 
-  toWebp(content, isWebp) {
-    const imgstr = isWebp ? 'imageMogr2/format/webp' : 'imageslim';
-    return content.replace(/<img\s+(.*?)src="([^"]+)"/g, (all, $0, src) => {
-      return `<img src="${src}?${imgstr}" ${$0}`;
-    });
-  },
-
-  toWebpUrl(url, isWebp) {
-    const imgstr = isWebp ? 'imageMogr2/format/webp' : 'imageslim';
-    return `${url}?${imgstr}`;
-  }
+    /**
+     * 图片链接转webp或者七牛图片压缩
+     *
+     * @param  {string}  url 链接
+     * @param  {boolean} isWebp  是否支持webp
+     *
+     * @return {string}
+     */
+    toWebpUrl(url, isWebp) {
+        const imgstr = isWebp ? 'imageMogr2/format/webp' : 'imageslim';
+        return `${url}?${imgstr}`;
+    }
 };
