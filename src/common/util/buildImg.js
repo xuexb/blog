d1495f88922a287f85f01c06e33e29480c734a1b
xuexb <fe.xiaowu@gmail.com>
2022-03-25 10:16:04 +0800

    fix: 修复图片占位没有 `alt`

--- src/common/util/buildImg.js
+++ src/common/util/buildImg.js
@@ -5,6 +5,13 @@
  */
 const placeholder = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
 
+/**
+ * 图片匹配正则，需要结合后台上传图片生成的规则
+ * 
+ * @type {RegExp}
+ */
+const reg_img = /<img\s+src="([^"]+)"\s*(.*?)width="(\d+)"\s+height="(\d+)"\/?>/g;
+
 export default {
     /**
      * 输出主站带有占位+延迟加载的img标签
@@ -14,8 +21,13 @@ export default {
      * @return {string}
      */
     lazy(content) {
-        return content.replace(/<img\s+src="([^"]+)"\s*(.*?)width="(\d+)"\s+height="(\d+)"\/?>/g, (all, src, alt, width, height) => {
-            return `<div class="img" style="width: ${width}px;"><i style="padding-bottom: ${height / width * 100}%"></i><img src="${placeholder}" data-src="${src}" class="lazy-load"></div>`;
+        return content.replace(reg_img, (all, src, alt, width, height) => {
+            return [
+                `<div class="img" style="width: ${width}px;">`,
+                    `<i style="padding-bottom: ${height / width * 100}%"></i>`,
+                    `<img width=${width} height=${height} src="${placeholder}" data-src="${src}" class="lazy-load" ${alt}>`,
+                '</div>'
+            ].join('');
         });
     },
 
