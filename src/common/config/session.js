098313833768c703afe74a4334b48869cf2ae338
xuexb <fe.xiaowu@gmail.com>
2022-05-27 22:23:34 +0800

    feat: 使用数据库存储 Session 标识，方便多机部署

--- src/common/config/session.js
+++ src/common/config/session.js
@@ -5,16 +5,11 @@
  */
 export default {
   name: 'thinkjs',
-  type: 'file',
+  type: 'db',
   secret: '!N71PV5J',
   timeout: 24 * 3600,
   cookie: { // cookie options
     length: 32,
     httponly: true
   },
-  adapter: {
-    file: {
-      path: think.getPath('common', 'runtime') + '/session',
-    }
-  }
 };
