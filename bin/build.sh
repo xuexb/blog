97b1cfb599e85c6e22bc31add788c7f01b245105
xuexb <fe.xiaowu@gmail.com>
2022-08-24 09:49:56 +0800

    chore: 添加构建时删除原始文件

--- bin/build.sh
+++ bin/build.sh
@@ -35,5 +35,8 @@ rm -rf output/app/common/runtime;
 #rm -rf output/www/static/admin/js/*.map;
 find output/ -name '*.map' | xargs rm;
 
+# 删除原始文件
+# find output/www/static/home -type f -regex ".*/[a-z0-9]*\.[a-z]*$" | xargs rm;
+
 # Service Worker
 yarn build:sw
