32d95ca364abbbf3a9d166a6eb18cbb2f6efd107
xuexb <fe.xiaowu@gmail.com>
2023-11-18 12:26:20 +0800

    chore: 删除文件

--- bin/copy_package.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var content = fs.readFileSync('./package.json', 'utf8');
-var data = JSON.parse(content);
-delete data.devDependencies;
-data.scripts = {
-  start: 'node www/production.js'
-};
-fs.writeFileSync('output/package.json', JSON.stringify(data, undefined, 4));
--- src/admin/config/route.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-export default [
-    // [/^admin\/(.+?)$/, ':1']
-];
